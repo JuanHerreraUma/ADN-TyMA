57c7af1c79714aa4c10d32c084ae251ee67a7146
Juan Antonio Herrera Conde <jaherreraconde@MacBook-Pro-de-Juan-JR.local>
2020-05-14 00:46:57 +0200

    Nucleotides' Frequency

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-#Juan Antonio Herrera Conde para TyMA. DNA Sequence.
-#Ejecutamos los métodos creados.
+# Juan Antonio Herrera Conde para TyMA. DNA Sequence.
+# Ejecutamos los métodos creados.
 
 from Metodos import *
 import random
@@ -10,12 +10,12 @@ secuencia = validarSecuencia(secuenciaAleatoria)
 
 print("Secuencia = ", secuencia)
 
-print ("La frecuencia de los nucleótidos es: ", contarFrecuenciaNuc(secuencia))
+print("Las frencuencias relativas de los nucleótidos es: ", contarFrecuenciaNuc(secuencia))
 
 print("Complementaria: ", Complementaria(secuencia))
 
 print(f"Secuencia ADN + Complementaria + Reversa Complementaria:\n5' {secuencia} 3'")
-print(f"   {''.join(['|' for c in range (len(secuencia))])}")
+print(f"   {''.join(['|' for c in range(len(secuencia))])}")
 print(f"3' {Complementaria(secuencia)} 5'")
 print(f"5' {reversaComplementaria(secuencia)} 3'\n")
 
@@ -29,6 +29,6 @@ print(codones(ARNmensajero))
 
 print("Secuencia de aminoácidos: ", secuenciaAminoacidos)
 
-print ("Marcos de lectura: ")
+print("Marcos de lectura: ")
 for marcos in marcosLectura(ARNmensajero):
     print(marcos)
--- Metodos.py
+++ Metodos.py
@@ -1,48 +1,64 @@
-#Juan Antonio Herrera Conde para TyMA. DNA Sequence.
+# Juan Antonio Herrera Conde para TyMA. DNA Sequence.
 
 Nucleotidos = ["A", "C", "G", "T"]
 Compl = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
-ADN_Codones = {"GCA":"A", "GCC":"A", "GCG":"A", "UGC":"C", "UGU":"C", "GAC":"D", "GAA":"E", "GAG":"E", "UUC":"F",
-                "GGA":"G", "GGC":"G", "GGG":"G", "CAC":"H", "CAU":"H", "AUA":"I", "AUU":"I", "AAA":"K", "AAG":"K",
-                "UUG":"L", "CUA":"L", "CUC":"L", "CUU":"L", "AUG":"M", "AAC":"N", "CCA":"P", "CCC":"P", "CCG":"P",
-                "CAA":"Q", "CAG":"Q", "AGA":"R", "CGA":"R", "CGC":"R", "CGU":"R", "AGC":"S", "AGU":"S", "UCA":"S",
-                "UCG":"S", "UCU":"S", "ACA":"T", "ACG":"T", "ACU":"T", "GUA":"V", "GUG":"V", "GUU":"V", "UGG":"W",
-                "UAU":"Y", "UAG":"_", "UAA":"_","GCU":"A", "GAU":"D", "UUU":"F", "GGU":"G", "AUC":"I", "UUA":"L",
-                "CUG":"L", "AAU":"N", "CCU":"P", "AGG":"R", "CGG":"R", "UCC":"S", "ACC":"T", "GUC":"V", "UAC":"Y",
-                "UGA":"_"}
+ADN_Codones = {"GCA": "A", "GCC": "A", "GCG": "A", "UGC": "C", "UGU": "C", "GAC": "D", "GAA": "E", "GAG": "E",
+               "UUC": "F",
+               "GGA": "G", "GGC": "G", "GGG": "G", "CAC": "H", "CAU": "H", "AUA": "I", "AUU": "I", "AAA": "K",
+               "AAG": "K",
+               "UUG": "L", "CUA": "L", "CUC": "L", "CUU": "L", "AUG": "M", "AAC": "N", "CCA": "P", "CCC": "P",
+               "CCG": "P",
+               "CAA": "Q", "CAG": "Q", "AGA": "R", "CGA": "R", "CGC": "R", "CGU": "R", "AGC": "S", "AGU": "S",
+               "UCA": "S",
+               "UCG": "S", "UCU": "S", "ACA": "T", "ACG": "T", "ACU": "T", "GUA": "V", "GUG": "V", "GUU": "V",
+               "UGG": "W",
+               "UAU": "Y", "UAG": "_", "UAA": "_", "GCU": "A", "GAU": "D", "UUU": "F", "GGU": "G", "AUC": "I",
+               "UUA": "L",
+               "CUG": "L", "AAU": "N", "CCU": "P", "AGG": "R", "CGG": "R", "UCC": "S", "ACC": "T", "GUC": "V",
+               "UAC": "Y",
+               "UGA": "_"}
 
 
-#Validamos la secuencia
+# Validamos la secuencia
 def validarSecuencia(dna_sec):
-    tmpseq = dna_sec.upper() #Necesitamos las letras en mayúsculas
+    tmpseq = dna_sec.upper()  # Necesitamos las letras en mayúsculas
     for nucleotidos in tmpseq:
         if nucleotidos not in nucleotidos:
             return False
     return tmpseq
 
-#Contar la frecuencia de los nucleótidos
+
+# Contar la frecuencia de los nucleótidos
 
 def contarFrecuenciaNuc(dna_sec):
     tmpFreq = {"A": 0, "C": 0, "G": 0, "T": 0}
     for nucleotidos in dna_sec:
         tmpFreq[nucleotidos] += 1
-    return tmpFreq
+
+    print("A=",(tmpFreq["A"] / len(dna_sec)) * 100,"%",
+          "C=",(tmpFreq["C"] / len(dna_sec)) * 100,"%",
+          "G=",(tmpFreq["G"] / len(dna_sec)) * 100,"%",
+          "T=",(tmpFreq["T"] / len(dna_sec)) * 100,"%",)
 
 def transcripcion(dna_seq):
-    #Proceso en el que el adn se transforma en arn antes de ser traducido a proteinas
-    return dna_seq.replace ("T", "U")
+    # Proceso en el que el adn se transforma en arn antes de ser traducido a proteinas
+    return dna_seq.replace("T", "U")
+
 
 def Complementaria(seq):
     return ''.join([Compl[nucleotidos] for nucleotidos in seq])
 
+
 def reversaComplementaria(seq):
     return ''.join([Compl[nucleotidos] for nucleotidos in seq])[::-1]
 
+
 def traduccion(seq, init_pos=0):
-    return [ADN_Codones[seq[pos:pos + 3]] for pos in range (init_pos, len(seq) -2, 3)]
+    return [ADN_Codones[seq[pos:pos + 3]] for pos in range(init_pos, len(seq) - 2, 3)]
+
 
 def marcosLectura(seq):
-    #Genera seis marcos de lectura
+    # Genera seis marcos de lectura
     marcos = []
     marcos.append(traduccion(seq, 0))
     marcos.append(traduccion(seq, 1))
@@ -71,11 +87,3 @@ def codones(seq):
             print("Un codon de parada!")
         elif (codon == "UGA"):
             print("Un codon de parada!")
-
-
-
-
-
-
-
-
